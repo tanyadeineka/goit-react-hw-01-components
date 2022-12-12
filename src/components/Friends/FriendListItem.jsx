1f56729b1c8fa4b128db0c839bba9fc1bdc0c58b
Tetiana Deineka <tata4ka666@i.ua>
2022-12-12 22:29:58 +0200

    Create FriendListItem.jsx

---
+++ src/components/Friends/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className="item">
+    <span className={isOnline ? "online" : "offline"}></span>
+        <img className="avatar" src={avatar} alt="User avatar" width="48" />
+        <p className="name">{name}</p>
+  </li>
+);
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
