05e000ec0351e9840204d7f2f50462c02ef9eb1c
Tetiana Deineka <tata4ka666@i.ua>
2022-12-12 23:22:04 +0200

    Update FriendList.jsx

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -1,17 +1,18 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
+import css from "./Friend.module.css";
 
 export const FriendList = ({ friends }) => (
-    <ul className="friend-list">
-        {friends.map(friend => (
-            <FriendListItem
-                avatar={friend.avatar}
-                name={friend.name}
-                key={friend.id}
-                isOnline={friend.isOnline}
-            ></FriendListItem>
-        ))}
-    </ul>
+  <ul className={css.friendList}>
+    {friends.map(friend => (
+      <FriendListItem
+        avatar={friend.avatar}
+        name={friend.name}
+        key={friend.id}
+        isOnline={friend.isOnline}
+      ></FriendListItem>
+    ))}
+  </ul>
 );
 
 FriendList.propTypes = {
