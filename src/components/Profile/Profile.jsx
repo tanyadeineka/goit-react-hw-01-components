a4ef710ad871645e60be1fe5d4efb7df704942f5
Tetiana Deineka <tata4ka666@i.ua>
2022-12-13 15:10:55 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -11,15 +11,15 @@ export const Profile = ({ username, tag, location, avatar, stats}) => {
         <p className={css.location}>{location}</p>
       </div>
       <ul className={css.stats}>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Followers</span>
           <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Views</span>
           <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Likes</span>
           <span className={css.quantity}>{stats.likes}</span>
         </li>
