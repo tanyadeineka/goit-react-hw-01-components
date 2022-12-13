e837c767787981a46ba783fb013231b74b8dc5b8
Tetiana Deineka <tata4ka666@i.ua>
2022-12-13 18:16:56 +0200

    Update TransactionHistory.jsx

--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -2,20 +2,20 @@ import PropTypes from 'prop-types';
 import css from "./Transactions.module.css";
 
 export const TransactionHistory = ({ items }) => (
-  <table className={css.transactionHistory}>
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
+  <table className={css.transactions}>
+    <thead className={css.headTable}>
+      <tr className={css.valueTable}>
+        <th className={css.nameRow}>TYPE</th>
+        <th className={css.nameRow}>AMOUNT</th>
+        <th className={css.nameRow}>CURRENCY</th>
       </tr>
     </thead>
     <tbody>
       {items.map(item => (
-        <tr key={item.id}>
-          <td>{item.type}</td>
-          <td>{item.amount}</td>
-          <td>{item.currency}</td>
+        <tr key={item.id} className={css.valueTable}>
+          <td className={css.value}>{item.type}</td>
+          <td className={css.value}>{item.amount}</td>
+          <td className={css.value}>{item.currency}</td>
         </tr>
       ))}
     </tbody>
